8ec1c7e3717d4e0e12c7db05def01fe76aeb8ed0
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-10 14:32:57 +0200

    static list

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,32 @@
 <template>
-    <header>
-        <h1>Header Content</h1>
+    <header class="header-content container">
+        <!-- BRAND LOGO -->
+        <div class="nav-left">
+            <a href="https://fontmeme.com/netflix-font/"
+                ><img
+                    src="https://fontmeme.com/permalink/210510/a43d586dbbb0088129005263a09c8438.png"
+                    alt="netflix-font"
+                    border="0"
+            /></a>
+            <!-- <ul class="menu-search">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Film</a></li>
+                <li><a href="#">Serie TV</a></li>
+                <li><a href="#">La mia lista</a></li>
+            </ul> -->
+        </div>
+        <div class="nav-right">
+            <div class="searchBox">
+                <form>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Search your title here..."
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -10,4 +36,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.header-content {
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+// Brand Logo
+.nav-left img {
+    width: 180px;
+}
+</style>
--- src/components/TopJumbo.vue
+++ src/components/TopJumbo.vue
@@ -1,12 +1,55 @@
 <template>
-    <section class="top-jumbo">
-        list film here
+    <section class="top-jumbo container">
+        <h2>Results</h2>
+
+        <ul>
+            <li v-for="(film, index) in filmList" :key="index">
+                <h3>{{ film.title }}</h3>
+                <h3>{{ film.original_title }}</h3>
+                <h3>{{ film.original_language }}</h3>
+                <h3>{{ film.vote_average }}</h3>
+
+                <!-- <p>{{ film.overview }}</p> -->
+            </li>
+        </ul>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Top-Jumbo',
+    data() {
+        return {
+            urlAPI: 'https://api.themoviedb.org/3/search/movie', // API
+            filmList: [], // Array che ospiterà API
+        };
+    },
+
+    created() {
+        this.getFilmList();
+    },
+
+    methods: {
+        getFilmList() {
+            // API CALL
+            axios
+                .get(this.urlAPI, {
+                    params: {
+                        api_key: 'ad7cef1cdf8a5fcf4e73a44c4f9e4bba',
+                        query: 'iron man',
+                    },
+                })
+                .then(res => {
+                    console.log(res.data.results);
+                    this.filmList = res.data.results;
+                    console.log(this.filmList);
+                })
+                .catch(err => {
+                    console.log('Errore', err);
+                });
+        },
+    },
 };
 </script>
 
