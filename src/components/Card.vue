c467c109909b66c5badea196be704cdba2d14bc0
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-11 17:02:45 +0200

    add stars

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="item">
+        <img
+            :src="`https://image.tmdb.org/t/p/w154${details.poster_path}`"
+            :alt="`Img of ${details.name} not found`"
+        />
         <ul class="list">
             <li>
                 Titolo:
@@ -21,7 +25,21 @@
                     alt=""
                 />
             </li>
-            <li>{{ details.vote_average }}</li>
+            <li>
+                <span
+                    v-for="(stars, index) in Math.round(
+                        details.vote_average / 2
+                    )"
+                    :key="index"
+                >
+                    <i class="fas fa-star"></i> </span
+                ><span
+                    v-for="(stars, i) in 5 -
+                        Math.round(details.vote_average / 2)"
+                    :key="i"
+                    ><i class="far fa-star"></i>
+                </span>
+            </li>
         </ul>
     </div>
 </template>
@@ -44,6 +62,9 @@ export default {
 .item {
     flex-basis: calc(100% / 5 - 2rem);
     margin: 1rem 1rem;
+    img {
+        max-width: 100%;
+    }
 }
 
 .list img {
